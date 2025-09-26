83178c1bbb393fa0e3fe2bf360e21aae79afc81b
hmrc-spifix <150905264+hmrc-spifix@users.noreply.github.com>
2025-09-26 12:19:51 +0100

    VPD-932 Check requests are coming from a user with a VPD enrolment, returning 401 when unauhtorised. (#1)

--- app/uk/gov/hmrc/vapingduty/Module.scala
+++ app/uk/gov/hmrc/vapingduty/Module.scala
@@ -1,7 +1,24 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.vapingduty
 
 import play.api.{Configuration, Environment}
-import play.api.inject.{Binding, Module => AppModule}
+import play.api.inject.{Binding, Module as AppModule}
+import uk.gov.hmrc.vapingduty.controllers.actions.{AuthorisedAction, BaseAuthorisedAction}
 
 import java.time.Clock
 
@@ -11,5 +28,6 @@ class Module extends AppModule:
     environment  : Environment,
     configuration: Configuration
   ): Seq[Binding[_]] =
-    bind[Clock].toInstance(Clock.systemDefaultZone) :: // inject if current time needs to be controlled in unit tests
+    bind[Clock].toInstance(Clock.systemDefaultZone) ::
+    bind[AuthorisedAction].to(classOf[BaseAuthorisedAction]) ::
     Nil
