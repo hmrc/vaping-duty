fbd41bd6edb275176de3a66b4a0b2f12801e6971
ghousedev <49005870+ghousedev@users.noreply.github.com>
2026-08-27 09:44:53 +0100

    VPD-1939: AI Assisted - Change circuit breaker implementation

---
+++ test/uk/gov/hmrc/vapingduty/connectors/NrsCircuitBreakerProviderSpec.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vapingduty.connectors
+
+import org.apache.pekko.actor.ActorSystem
+import org.apache.pekko.pattern.CircuitBreaker
+import play.api.Configuration
+import uk.gov.hmrc.vapingduty.base.SpecBase
+
+import scala.concurrent.duration._
+
+class NrsCircuitBreakerProviderSpec extends SpecBase {
+
+  private val actorSystem = ActorSystem("test-system")
+
+  private val testConfig = Configuration(
+    "microservice.services.nrs.max-failures"                -> 5,
+    "microservice.services.nrs.call-timeout"                -> "30 seconds",
+    "microservice.services.nrs.reset-timeout"               -> "60 seconds",
+    "microservice.services.nrs.max-reset-timeout"           -> "300 seconds",
+    "microservice.services.nrs.exponential-backoff-factor"  -> 2.0
+  )
+
+  override def afterAll(): Unit = {
+    actorSystem.terminate()
+    super.afterAll()
+  }
+
+  "NrsCircuitBreakerProvider" - {
+    "must create a NrsCircuitBreaker with correct configuration" in {
+      val provider = new NrsCircuitBreakerProvider(testConfig, actorSystem)
+      val nrsCircuitBreaker = provider.get()
+
+      nrsCircuitBreaker mustBe a[NrsConnector.NrsCircuitBreaker]
+      nrsCircuitBreaker.breaker mustBe a[CircuitBreaker]
+    }
+
+    "must return the same instance on multiple calls" in {
+      val provider = new NrsCircuitBreakerProvider(testConfig, actorSystem)
+      val breaker1 = provider.get()
+      val breaker2 = provider.get()
+
+      breaker1 mustBe breaker2
+    }
+
+    "must configure circuit breaker with values from configuration" in {
+      val provider = new NrsCircuitBreakerProvider(testConfig, actorSystem)
+      val nrsCircuitBreaker = provider.get()
+
+      // Circuit breaker is created successfully with config values
+      nrsCircuitBreaker.breaker mustBe a[CircuitBreaker]
+    }
+  }
+}
